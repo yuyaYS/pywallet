14db538e2d49d72d42cddc25b7593f21cfd33680
yuya <y303sushi@gmail.com>
2021-08-11 18:11:34 +0900

    nonce complete

--- blockchain.py
+++ blockchain.py
@@ -6,6 +6,8 @@ import utils
 import hashlib
 import json
 
+Mining_DIFFCULTY = 3
+
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 class Blockchain(object):
@@ -25,23 +27,53 @@ class Blockchain(object):
         self.chain.append(block)
         self.transaction_pool = []
         return block
+    
     def hash(self, block):
-        sorted_block = json.dumps(block, sort_keys= True, default=str)
+        sorted_block = json.dumps(block, sort_keys= True)
         return hashlib.sha256(sorted_block.encode()).hexdigest()
 
-def pprint(chains):
-    for i, chain in enumerate(chains):
-        print(f'{"-"*25} Chain{ i} {"-"*25}')
-        for k ,v in chain.items():
-            print(f'{k:15}{v}')
-    print(f'{"="*25}')
+    def add_transaction(self, sender_blockchain_address, 
+                    receipient_blockchain_address, value):
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': sender_blockchain_address,
+            'receipient_blockchain_address': receipient_blockchain_address,
+            'value': float(value)
+        })
+        self.transaction_pool.append(transaction)
+        return True
+
+    def valid_proof(self, transactions,previous_hash, 
+                    nonce, difficulty= Mining_DIFFCULTY):
+        guess_block =utils.sorted_dict_by_key({
+                        'timestamp': time.time(),
+            'transactions': transactions,
+            'nonce': nonce,
+            'previous_hash': previous_hash
+        })
+        guess_hash = self.hash(guess_block)
+        return guess_hash[:difficulty] == '0'*difficulty
+
+    def proof_of_work(self):
+        transactions = self.transaction_pool.copy()
+        previous_hash = self.hash(self.chain[-1])
+        nonce = 0
+        while self.valid_proof(transactions, previous_hash, nonce) is False:
+            nonce +=1
+        return nonce
+
+
 
 if __name__ == '__main__':
     block_chain = Blockchain()
-    pprint(block_chain.chain)
-    
-    previous_hash = block_chain.hash(block_chain.chain[-1])
+    utils.pprint(block_chain.chain)
 
-    block_chain.create_block(3,previous_hash)
-    pprint(block_chain.chain)
-    
+    block_chain.add_transaction('A','b', 1.0)
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    nonce = block_chain.proof_of_work()
+    block_chain.create_block(nonce, previous_hash)
+
+    block_chain.add_transaction('c','f', 21.0)
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    nonce = block_chain.proof_of_work()
+    block_chain.create_block(nonce, previous_hash)
+    utils.pprint(block_chain.chain)
---
+++ note.md
@@ -0,0 +1,11 @@
+In this code, each block is consited of four components.
+1. timestamp, 2. transactions, 3. nonce, 4, previous_hash.
+
+---How to find nonce---.
+Nonce = chanllege1 + previous hash + transcation
+challenge1 is the potentail number for nonce.
+
+Assming after you calculate Nonce for the first time, it was 23vf3ninidnvn32ini.
+We will continue to do more chanllege untill the first four digit is 0000(depending on dificulity, you can setup more long digit like 0000.....00000).
+
+In chanllege 50, the nounce was 0000bn4fnnjnn3nbfenfkefkn. GOOD, now we have nouce 50.
