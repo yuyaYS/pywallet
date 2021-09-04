0304d5b9cb54d17c52f1b4d15a369f98f049e6cc
yuya <y303sushi@gmail.com>
2021-09-04 22:21:19 +0900

    docs added

--- blockchain.py
+++ blockchain.py
@@ -7,15 +7,26 @@ import hashlib
 import json
 
 Mining_DIFFCULTY = 3
+MINING_SENDER = 'n23n3b3b4( senders adress)'
+Mining_REWARD = 1.3 
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 class Blockchain(object):
-
-    def __init__(self):    
+    '''
+    Blockchain class
+    ----------
+    transaction_pool : list
+        store the list of transaction 
+    chain : list
+        store the list of block(s)
+    '''
+    def __init__(self, blockchain_address = None):    
         self.transaction_pool = []
         self.chain = []
         self.create_block(0, self.hash({}))
+        self.blockchain_address = blockchain_address
 
     def create_block(self, nonce, previous_hash):
         block = utils.sorted_dict_by_key({
@@ -44,8 +55,11 @@ class Blockchain(object):
 
     def valid_proof(self, transactions,previous_hash, 
                     nonce, difficulty= Mining_DIFFCULTY):
+        '''This fucntion calcualte the nonce 
+            .
+        '''
         guess_block =utils.sorted_dict_by_key({
-                        'timestamp': time.time(),
+            'timestamp': time.time(),
             'transactions': transactions,
             'nonce': nonce,
             'previous_hash': previous_hash
@@ -54,6 +68,12 @@ class Blockchain(object):
         return guess_hash[:difficulty] == '0'*difficulty
 
     def proof_of_work(self):
+        """The fucntion calculates nonce 
+            untill it matchs given length of first digit of number.
+
+        Returns:
+            [type]: [description]
+        """
         transactions = self.transaction_pool.copy()
         previous_hash = self.hash(self.chain[-1])
         nonce = 0
@@ -61,19 +81,38 @@ class Blockchain(object):
             nonce +=1
         return nonce
 
+    def mining(self):
+        """Function
+        """
+        self.add_transaction(
+            sender_blockchain_address= MINING_SENDER,
+            receipient_blockchain_address = self.blockchain_address,
+            value= Mining_REWARD
+        )
+        nonce = self.proof_of_work()
+        previous_hash = self.hash(self.chain[-1])
+        self.create_block(nonce, previous_hash)
+        logger.info({"action":'mining', 'status':'sucess'})
+
 
 
 if __name__ == '__main__':
-    block_chain = Blockchain()
+    my_blockchain_address = '3njr36v9w3(your address)'
+    block_chain = Blockchain(blockchain_address=my_blockchain_address)
     utils.pprint(block_chain.chain)
 
     block_chain.add_transaction('A','b', 1.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    nonce = block_chain.proof_of_work()
-    block_chain.create_block(nonce, previous_hash)
+    block_chain.mining()
+    print("現在の hash "+ block_chain.hash(block_chain.chain[0]))
+    # previous_hash = block_chain.hash(block_chain.chain[-1])
+    # nonce = block_chain.proof_of_work()
+    # block_chain.create_block(nonce, previous_hash)
 
     block_chain.add_transaction('c','f', 21.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    nonce = block_chain.proof_of_work()
-    block_chain.create_block(nonce, previous_hash)
+    block_chain.mining()
+    print("現在の hash "+ block_chain.hash(block_chain.chain[-2]))
+
+    # previous_hash = block_chain.hash(block_chain.chain[-1])
+    # nonce = block_chain.proof_of_work()
+    # block_chain.create_block(nonce, previous_hash)
     utils.pprint(block_chain.chain)
