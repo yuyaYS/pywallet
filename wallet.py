cf884440b302d5831a010bd6c5b20dc48d96d165
Yuya Shimbori <y303sushi@gmail.com>
2022-09-29 10:42:54 -0700

    add generating blockchain address algo

--- wallet.py
+++ wallet.py
@@ -1,3 +1,4 @@
+import binascii
 from ecdsa import NIST256p
 from ecdsa import SigningKey
 import base58
@@ -9,6 +10,7 @@ class Wallet(object):
     def __init__(self) -> None:
         self._private_key = SigningKey.generate(curve = NIST256p)
         self._public_key =self._private_key.get_verifying_key()
+        self._blockchain_address = self.generate_blockchain_address()
 
     @property
     def private_key(self):
@@ -18,8 +20,12 @@ class Wallet(object):
     def public_key(self):
         return self._public_key.to_string().hex()
 
+    @property
+    def blockchain_address(self):
+        return self._blockchain_address
 
     def generate_blockchain_address(self):
+        #byte private key
         public_key_bytes = self._public_key.to_string()
         sha256_bpk = hashlib.sha256(public_key_bytes)
         sha256_bpk_digest = sha256_bpk.digest()
@@ -29,7 +35,29 @@ class Wallet(object):
         ripemed160_bpk_digest = ripemed160_bpk.digest()
         ripemed160_bpk_hex = codecs.encode(ripemed160_bpk_digest, 'hex')
 
+        network_byte = b'00'
+        network_bitcoin_pk = network_byte + ripemed160_bpk_hex
+        network_bitcoin_pk_bytes = codecs.decode(
+            network_bitcoin_pk, 'hex'
+        )
+
+        sha256_bpk = hashlib.sha256(network_bitcoin_pk_bytes)
+        sha256_bpk_digest = sha256_bpk.digest()
+
+        sha256_2_nbpk = hashlib.sha256(sha256_bpk_digest)
+        sha256_2_nbpk_digest = sha256_2_nbpk.digest()
+        sha256_hex =  codecs.encode(sha256_2_nbpk_digest, 'hex')
+
+        checksum = sha256_hex[:8]
+        address_hex = (network_bitcoin_pk + checksum).decode('utf-8')
+
+        blockchain_address = base58.b58encode(binascii.unhexlify(address_hex)).decode('utf-8')
+        return blockchain_address
+
+
 if __name__=='__main__':
     wallet = Wallet()
     print(wallet.private_key)
     print(wallet.public_key)
+
+    print(wallet.blockchain_address)
